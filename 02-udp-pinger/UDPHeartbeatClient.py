2f802c873c41793341322c47cde4819c17391605
valdirjunior <valdirrugiskijr@gmail.com>
2025-10-27 17:40:18 -0300

    ajustes no heartbeat

--- 02-udp-pinger/UDPHeartbeatClient.py
+++ 02-udp-pinger/UDPHeartbeatClient.py
@@ -1,14 +1,14 @@
 import socket
 import time
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverAddress = ('127.0.0.1', 12001) # Porta diferente do Ping para este serviço
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Criar um socket UDP IPv4
+serverAddress = ('127.0.0.1', 12001)  # Porta diferente do Ping para este serviço
 
 try:
     while True:
         sequence_number = int(time.time())  # Usar timestamp como número de sequência
         heartbeat_message = f"Heartbeat {sequence_number} {time.time()}"
-        clientSocket.sendto(heartbeat_message.encode(), serverAddress)
+        clientSocket.sendto(heartbeat_message.encode(), serverAddress)  # Enviar heartbeat para o servidor
         print(f"Heartbeat enviado: {heartbeat_message}")
         time.sleep(5)  # Enviar heartbeat a cada 5 segundos
 
--- 02-udp-pinger/UDPHeartbeatServer.py
+++ 02-udp-pinger/UDPHeartbeatServer.py
@@ -3,29 +3,41 @@ import time
 from threading import Thread, Lock
 
 UDP_PORT = 12001  # Porta para o serviço de heartbeat
-HEARTBEAT_TIMEOUT = 15  # Tempo em segundos para considerar oa perda do heartbeat
+HEARTBEAT_TIMEOUT = 15  # Tempo em segundos para considerar a perda do heartbeat
 
-heartbeats = {}
-lock = Lock()
+heartbeats = {}  # Dicionário para armazenar os últimos tempos de heartbeat por cliente
+lock = Lock()  # Para proteger o acesso ao dicionário de heartbeats
 
 def cleanup_heartbeats():
+    # 
     while True:
         time.sleep(5)
         with lock:
             now = time.time()
+
+            # Remover clientes inativos do dicionário
             inactive = [ip for ip, last_time in heartbeats.items() if now - last_time > HEARTBEAT_TIMEOUT]
+
+            # Log dos clientes inativos
             for ip in inactive:
                 print(f"Cliente {ip} não enviou heartbeat nos últimos {HEARTBEAT_TIMEOUT} segundos. Considerando como perdido.")
-                del heartbeats[ip]
+                del heartbeats[ip]  # Remover do dicionário
 
+# Função para o servidor de heartbeat
 def servidor_heartbeat():
+
+    # Criar socket UDP
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Atribuir endereço IP e número da porta ao socket
     serverSocket.bind(('', UDP_PORT))
     print(f"Servidor Heartbeat escutando na porta {UDP_PORT}")
 
+    # Iniciar thread para limpeza de heartbeats inativos
     cleanup_thread = Thread(target=cleanup_heartbeats, daemon=True)
     cleanup_thread.start()
 
+    # Loop principal para receber heartbeats
     while True:
         data, addr = serverSocket.recvfrom(1024)
         message = data.decode()
